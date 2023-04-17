14ba9ccb26b0c3921bf840209d5fc44aef5948b0
Anna Peterson <reveragb@gmail.com>
2023-04-17 10:39:34 -0600

    Add page files

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+// '/'
+
+// import Image from "next/image";
+// import styles from "./page.module.css";
+// import imageStyles from "./styles/images.module.css";
+// import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <main className='main'>
+      <h1>Hello</h1>
+    </main>
+  )
+}
