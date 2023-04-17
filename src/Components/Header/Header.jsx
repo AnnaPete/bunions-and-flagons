31f1d11470c18a6071a116e2a1b1197208a4aced
Anna Peterson <reveragb@gmail.com>
2023-04-17 13:31:52 -0600

    Add changes

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import './Header.module.css'
+import React from 'react'
+import title from '../Images/title.png'
+import Link from 'next/link'
+// import PropTypes from 'prop-types'
+
+const Header = ( { data } ) => {
+  const renderLinks = () => {
+    if (!data) {
+      return null
+    }
+
+    return Object.keys(data).map((key) => (
+      <NavLink to={`/${key}`} key={key}>{key}</NavLink>
+    ))
+  }
+
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'>
+          <img className='dnd-title' src={title} alt='dungeons and dragons' />
+        </Link>
+      </div>
+      <div className='title-container'>
+        <span className='title'>The Book of Many Things</span>
+        <nav id='nav-bar'>
+          {renderLinks()}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+// 
+// Header.propTypes = {
+//   props: PropTypes.shape({
+//     data: PropTypes.shape({
+//       key: PropTypes.string.isRequired
+//     })
+//   })
+// }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,31 @@
 // '/'
 
-// import Image from "next/image";
-// import styles from "./page.module.css";
+import Image from "next/image";
+import image from '../Components/Images/dungeons-and-dragons-lede.jpg'
+import styles from "./page.module.css";
 // import imageStyles from "./styles/images.module.css";
 // import Link from "next/link";
+import Header from '../Components/Header/Header'
 
 
 export default function Home() {
   return (
     <main className='main'>
-      <h1>Hello</h1>
+      <Header />
+      <div className="about-section">
+        <h1 className="tagline">Let the dice decide</h1>
+        <div className="about-description">
+          <div className="about-text">
+            <div className="about-text">
+              <h2>What exactly IS Dungeons & Dragons?</h2>
+              <p>Dungeons & Dragons (D&D) is a fantasy tabletop role-playing game where players create their own characters and go on adventures guided by a storyteller, called the Dungeon Master (DM).</p>
+              <p>The game involves rolling dice to determine the outcome of various actions and decisions made by the players. D&D allows for a great deal of creativity and imagination as players work together to solve puzzles, fight monsters, and explore fantastical worlds.</p>
+              <p>It's a collaborative storytelling experience where anything can happen and the possibilities are endless.</p>
+            </div>
+            <Image className="intro" src={image} alt="adventurers fighting a dragon" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
